fec915ff1121bbd3e7100fa494a6a9464635cfaf
AlinaS <602604@stud.hvl.no>
2023-10-25 09:31:40 +0200

    slette terreng koden

--- threejs-template-master/js/main.js
+++ threejs-template-master/js/main.js
@@ -90,55 +90,10 @@ async function main() {
     camera.position.y = 20;
     camera.rotation.x -= Math.PI * 0.25;
 
-
     /**
-     * Add terrain:
-     *
-     * We have to wait for the image file to be loaded by the browser.
-     * There are many ways to handle asynchronous flow in your application.
-     * We are using the async/await language constructs of Javascript:
-     *  - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/async_function
+     * add 3D model fjell
      */
-    const heightmapImage = await Utilities.loadImage('resources/images/vesuvioheightmap2.jpg');
-    const width = 100;
 
-    const simplex = new SimplexNoise();
-    const terrainGeometry = new TerrainBufferGeometry({
-        width,
-        heightmapImage,
-        // noiseFn: simplex.noise.bind(simplex),
-        numberOfSubdivisions: 128,
-        height: 30
-    });
-
-    const grassTexture = new TextureLoader().load('resources/textures/grass_02.png');
-    grassTexture.wrapS = RepeatWrapping;
-    grassTexture.wrapT = RepeatWrapping;
-    grassTexture.repeat.set(5000 / width, 5000 / width);
-
-    const snowyRockTexture = new TextureLoader().load('resources/textures/snowy_rock_01.png');
-    snowyRockTexture.wrapS = RepeatWrapping;
-    snowyRockTexture.wrapT = RepeatWrapping;
-    snowyRockTexture.repeat.set(1500 / width, 1500 / width);
-
-
-    const splatMap = new TextureLoader().load('resources/images/splatmap_01.png');
-
-    const terrainMaterial = new TextureSplattingMaterial({
-        color: 0xffffff,
-        shininess: 0,
-        textures: [snowyRockTexture, grassTexture],
-        splatMaps: [splatMap]
-    });
-
-    const terrain = new Mesh(terrainGeometry, terrainMaterial);
-
-    terrain.castShadow = true;
-    terrain.receiveShadow = true;
-
-    terrain.position.y = 0;
-
-    scene.add(terrain);
 
 
 // Water
